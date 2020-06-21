09871de2762df15b996de26945b31d731970e907
mikelyt777 <mikelyt777@users.noreply.github.com>
2020-06-21 12:25:45 +0800

    Update 2020-06-21T04:25:19.793Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,4 +1,21 @@
 "use strict";
 
-$(document).ready(function () {});
+$(document).ready(function () {
+  $('.assign_object').click(function (e) {
+    event.preventDefault();
+    $('.assign_object').removeClass('active');
+    $(this).toggleClass('active');
+  });
+  $('.sidebar_li').click(function (e) {
+    event.preventDefault();
+    $('.sidebar_li').removeClass('active');
+    $(this).toggleClass('active');
+  });
+  $('.green_reply').click(function (e) {
+    $('.reply_section').show();
+  });
+  $('.white_cancel').click(function (e) {
+    $('.reply_section').hide();
+  });
+});
 //# sourceMappingURL=all.js.map
