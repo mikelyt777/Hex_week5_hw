9148a59d86f5b1ab8d073728d928fcbb2107994f
mikelyt777 <mikelyt777@users.noreply.github.com>
2020-06-21 11:53:01 +0800

    finished the replying thing

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,23 @@
-$(document).ready(() => {
+$(document).ready(function(){
 
+    $('.assign_object').click(function(e) {
+        event.preventDefault();
+        $('.assign_object').removeClass('active');
+        $(this).toggleClass('active');
+    })
+
+    $('.sidebar_li').click(function(e) {
+        event.preventDefault();
+        $('.sidebar_li').removeClass('active');
+        $(this).toggleClass('active');
+    })
+
+    $('.green_reply').click(function(e) {
+        $('.reply_section').show();
+    })
+
+    $('.white_cancel').click(function(e) {
+        $('.reply_section').hide();
+    })
+  
 });
